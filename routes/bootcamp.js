fcd56b47a5490aa82d54c6bdb46437ee98b20afe
xboudsady <boudsadyx@gmail.com>
2020-03-29 16:23:43 -0700

    Mounted router, updated route path

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -2,26 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 // Get all bootcamps
-router.get('/api/v1/bootcamps', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({ success: true, msg: 'Show all boocamps' });
 });
 
 // Show single bootcamp
-router.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
 });
 
 // Add a bootcamp
-router.post('/api/v1/bootcamps', (req, res) => {
+router.post('/', (req, res) => {
     res.status(200).json({ success: true, msg: 'Create new bootcamp'})
 });
 
 // Update a single bootcamp
-router.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
 // Delete a single bootcamp
-router.delete('/api/v1/bootcamps/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Loads our route files from folder
+const bootcamps = require('./routes/bootcamp');
+
+
 dotenv.config({ path: './config/config.env' });     // Load env vars
 
 // Initialize our application with express
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 
 // Load environment variables
 const PORT = process.env.PORT || 50000;
