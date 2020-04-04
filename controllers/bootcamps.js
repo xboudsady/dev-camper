ddc6f274288ef25e1a8cd05894ae0967b9ff1eae
xboudsady <boudsadyx@gmail.com>
2020-04-04 15:01:55 -0700

    Added error hangline for ID parameters

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -20,6 +20,10 @@ exports.getBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
         res.status(200).json({ success: true, data: bootcamp });
     } catch (error) {
         res.status(400).json({ success: false });
