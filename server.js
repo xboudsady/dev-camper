fd290031cead0e27a942469df50ef16423ddc5f3
xboudsady <boudsadyx@gmail.com>
2020-04-03 20:31:13 -0700

    Added morgan

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 
 // Load environment variables
-const PORT = process.env.PORT || 50000;
+const PORT = process.env.PORT || 5000;
 
 // Call our server, (port, callback function())
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
