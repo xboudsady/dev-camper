aee261059df54a2835fdaf6ddaab6851bf07054e
xboudsady <boudsadyx@gmail.com>
2020-04-09 20:11:04 -0700

    fix: variable mispelling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse');
-const asyncHanlder = require('../middleware/async');
+const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
@@ -16,7 +16,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = asyncHanlder(async (req, res, next) => {
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
@@ -31,7 +31,7 @@ exports.getBootcamp = asyncHanlder(async (req, res, next) => {
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = asyncHanlder(async (req, res, next) => {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.create(req.body);
 
     res.status(201).json({
@@ -43,7 +43,7 @@ exports.createBootcamp = asyncHanlder(async (req, res, next) => {
 // @desc    Update new botcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = asyncHanlder(async (req, res, next) => {
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true
@@ -61,7 +61,7 @@ exports.updateBootcamp = asyncHanlder(async (req, res, next) => {
 // @desc    Delete new botcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = asyncHanlder(async (req, res, next) => {
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
