64f5a469f6bb874884ef2a28e9dc2cf28341767b
xboudsady <boudsadyx@gmail.com>
2020-04-09 20:32:32 -0700

    Added slugify to model, and pre() method

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
    name: {
@@ -99,5 +100,12 @@ const BootcampSchema = new mongoose.Schema({
        default: Date.now
    }
 });
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function(next) {
+    console.log('Slugify ran', this.name);
+    next();
+});
+
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
