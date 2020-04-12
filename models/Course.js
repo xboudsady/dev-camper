a8a9f116922f2f1200d104e96b3f4202e11e4890
xboudsady <boudsadyx@gmail.com>
2020-04-12 11:04:21 -0700

    feature: Created Courses database model

---
+++ models/Course.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const CoursSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a course title']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description']
+    },
+    weeks: {
+        type: String,
+        required: [true, 'Please add number of weeks']
+    },
+    tuition: {
+        type: Number,
+        required: [true, 'Please add a tuition cost']
+    },
+    minimumSkill: {
+        type: String,
+        required: [true, 'Please add a minimum skill'],
+        enum: ['beginner', 'intermediate', 'advanced']
+    },
+    scholarshipAvailable: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
